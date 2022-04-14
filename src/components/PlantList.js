ce8ccdfd94e7f4f88b2b80e0e95787f5bf7c1011
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-04-14 14:37:05 -0400

    commit tree 6d66fccdb7605105516250c8745d2b47368a6cb0

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,18 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+function PlantList({ plantsToDisplay }) {
+  const displayPlants = plantsToDisplay.map((plant) => (
+    <PlantCard
+      key={plant.id}
+      id={plant.id}
+      name={plant.name}
+      image={plant.image}
+      price={plant.price}
+    />
+  ));
+
+  return <ul className="cards">{displayPlants}</ul>;
 }
 
 export default PlantList;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPlants(data);
+        console.log(data);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  function handleNewPlant(newPlant) {
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlant),
+    })
+      .then((resp) => resp.json())
+      .then((postedPlant) => setPlants([...plants, postedPlant]));
+  }
+
+  const plantsToDisplay = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onFormSubmit={handleNewPlant} />
+      <Search onSearch={setSearchTerm} />
+      {isLoaded ? (
+        <PlantList plantsToDisplay={plantsToDisplay} />
+      ) : (
+        "Loading..."
+      )}
     </main>
   );
 }
